5e8ba716c4ce1edef2340c309f3dc55299cacf1c
S-jooyoung <jooyoung.dev@gmail.com>
2023-02-20 23:36:18 +0900

    Chore: 컨텐츠 상단 구글 애드센스 추가

--- src/components/post-content/index.js
+++ src/components/post-content/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './style.scss';
-
+import AdSense from '../adsense';
 function PostContent({ html }) {
   return (
     <div className="post-content">
+      <AdSense client="ca-pub-4083591465738564" slot="4148110521" />
       <div className="markdown" dangerouslySetInnerHTML={{ __html: html }} />
     </div>
   );
