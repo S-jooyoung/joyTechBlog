857177b023f7d931e953e53066dd4b463418af6d
shin jooyoung <young@staybility.co.kr>
2023-04-11 15:58:00 +0900

    Chore: 메타 태그 수정
    * 콘텐츠 페이지 description 태그 길이 80으로 수정

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,7 +39,7 @@ export const pageQuery = graphql`
       edges {
         node {
           id
-          excerpt(pruneLength: 500, truncate: true)
+          excerpt(pruneLength: 80, truncate: true)
           frontmatter {
             categories
             title
--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -50,7 +50,7 @@ export const pageQuery = graphql`
     cur: markdownRemark(fields: { slug: { eq: $slug } }) {
       id
       html
-      excerpt(pruneLength: 500, truncate: true)
+      excerpt(pruneLength: 80, truncate: true)
       frontmatter {
         date(formatString: "MMMM DD, YYYY")
         title
