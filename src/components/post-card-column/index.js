3880289f2fd46ce9bb1095aeb8475aafaaa855d3
S-jooyoung <jooyoung.dev@gmail.com>
2023-04-15 00:19:34 +0900

    Style: 메인화면 더 보기란 스타일 수정
    * 색상 변경 및 애니메이션 추가

--- src/components/post-card-column/index.js
+++ src/components/post-card-column/index.js
@@ -22,7 +22,7 @@ function PostCardColumn({ posts, showMoreButton, moreUrl }) {
             variant="contained"
             disableElevation
           >
-            More
+            더 보기
           </Button>
         )}
       </div>
