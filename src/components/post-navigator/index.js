83dbc67b5128d07b2c5ae71ef9656af299624aee
S-jooyoung <jooyoung.dev@gmail.com>
2023-05-11 01:14:03 +0900

    Style: post-navigator 디자인 변경

--- src/components/post-navigator/index.js
+++ src/components/post-navigator/index.js
@@ -1,25 +1,32 @@
 import { Link } from 'gatsby';
 import React from 'react';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import './style.scss';
 
 function PostNavigator({ prevPost, nextPost }) {
+  const prevThumbnailSrc = getImage(prevPost.thumbnail);
+  const nextThumbnailSrc = getImage(nextPost.thumbnail);
   return (
-    <div className="post-navigator">
-      <div className="post-navigator-card-wrapper">
-        {nextPost && (
-          <Link className="post-card prev" key={nextPost.id} to={nextPost.slug}>
-            <div className="direction">이전 글</div>
-            <div className="title">{nextPost.title}</div>
-          </Link>
-        )}
-      </div>
-      <div className="post-navigator-card-wrapper">
-        {prevPost && (
-          <Link className="post-card next" key={prevPost.id} to={prevPost.slug}>
-            <div className="direction">다음 글</div>
-            <div className="title">{prevPost.title}</div>
-          </Link>
-        )}
+    <div className="post-navigator-wrapper">
+      <div className="post-navigator">
+        <div className="post-navigator-card-wrapper">
+          {nextPost && (
+            <Link className="post-card prev" key={nextPost.id} to={nextPost.slug}>
+              <div className="direction">이전 글</div>
+              <GatsbyImage className="thumbnail" image={prevThumbnailSrc} alt="thumbnail" />
+              <div className="title">{nextPost.title}</div>
+            </Link>
+          )}
+        </div>
+        <div className="post-navigator-card-wrapper">
+          {prevPost && (
+            <Link className="post-card next" key={prevPost.id} to={prevPost.slug}>
+              <div className="direction">다음 글</div>
+              <GatsbyImage className="thumbnail" image={nextThumbnailSrc} alt="thumbnail" />
+              <div className="title">{prevPost.title}</div>
+            </Link>
+          )}
+        </div>
       </div>
     </div>
   );
