576eb5e25b5849cbc788715392462b9eba7eef22
S-jooyoung <jooyoung.dev@gmail.com>
2023-05-11 02:09:26 +0900

    Fix: 이미지 border-radius 미적용 버그 해결 && token-auth-principle 이름 변경

--- src/components/post-navigator/index.js
+++ src/components/post-navigator/index.js
@@ -4,29 +4,29 @@ import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import './style.scss';
 
 function PostNavigator({ prevPost, nextPost }) {
-  const prevThumbnailSrc = getImage(prevPost.thumbnail);
-  const nextThumbnailSrc = getImage(nextPost.thumbnail);
+  const prevThumbnailSrc = prevPost && getImage(prevPost.thumbnail);
+  const nextThumbnailSrc = nextPost && getImage(nextPost.thumbnail);
+
+  console.log(nextThumbnailSrc);
   return (
-    <div className="post-navigator-wrapper">
-      <div className="post-navigator">
-        <div className="post-navigator-card-wrapper">
-          {nextPost && (
-            <Link className="post-card prev" key={nextPost.id} to={nextPost.slug}>
-              <div className="direction">이전 글</div>
-              <GatsbyImage className="thumbnail" image={prevThumbnailSrc} alt="thumbnail" />
-              <div className="title">{nextPost.title}</div>
-            </Link>
-          )}
-        </div>
-        <div className="post-navigator-card-wrapper">
-          {prevPost && (
-            <Link className="post-card next" key={prevPost.id} to={prevPost.slug}>
-              <div className="direction">다음 글</div>
-              <GatsbyImage className="thumbnail" image={nextThumbnailSrc} alt="thumbnail" />
-              <div className="title">{prevPost.title}</div>
-            </Link>
-          )}
-        </div>
+    <div className="post-navigator">
+      <div className="post-navigator-card-wrapper">
+        {nextPost && (
+          <Link className="post-card prev" key={nextPost.id} to={nextPost.slug}>
+            <div className="direction">이전 글</div>
+            <GatsbyImage className="thumbnail" image={nextThumbnailSrc} alt="prevImage" />
+            <div className="title">{nextPost.title}</div>
+          </Link>
+        )}
+      </div>
+      <div className="post-navigator-card-wrapper">
+        {prevPost && (
+          <Link className="post-card next" key={prevPost.id} to={prevPost.slug}>
+            <div className="direction">다음 글</div>
+            <GatsbyImage className="thumbnail" image={prevThumbnailSrc} alt="nextImage" />
+            <div className="title">{prevPost.title}</div>
+          </Link>
+        )}
       </div>
     </div>
   );
