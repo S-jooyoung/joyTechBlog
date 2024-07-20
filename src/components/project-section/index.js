ccf761639f9753d2eb444009750ae78205dd1d2e
S-jooyoung <jooyoung.dev@gmail.com>
2024-07-21 02:35:30 +0900

    Design: about 페이지 디자인 수정 및 컨텐츠 추가
    - 자기 소개 컴포넌트 추가 및 내용 업데이트
    - 회사 경험 컴포넌트 추가 및 내용 업데이트
    - 프로젝트 경험 컴포넌트 추가 및 내용 업데이트

--- src/components/project-section/index.js
+++ src/components/project-section/index.js
@@ -4,37 +4,58 @@ import IconButtonBar from '../icon-button-bar';
 import Image from '../image';
 import './style.scss';
 
-function ProjectSection({ projects }) {
+function ProjectSection({ title, projects }) {
   if (!projects || projects.length < 2) return null;
   return (
     <div className="project-section">
-      <SectionHeader title="Projects" />
-      {projects.map((project, index) =>
-        index === 0 ? null : (
-          <div className="project" key={index}>
-            <div className="head">
-              {project.title}&nbsp;&nbsp;
-              {project.links && (
-                <IconButtonBar links={project.links} style={{ color: '#a8a8a8', fontSize: 24 }} />
-              )}
-            </div>
-            <div className="body">
-              <Image className="thumbnail" src={project.thumbnailUrl} />
-
-              {project.techStack && (
-                <div className="tech-stack">
-                  {project.techStack.map((tech, index) => (
-                    <div key={index} className="tech">
-                      {tech}
-                    </div>
-                  ))}
+      <SectionHeader title={title} />
+      <div className="project-wrapper">
+        {projects.map((project, index) =>
+          index === 0 ? null : (
+            <div className="project" key={index}>
+              <div className="body">
+                <Image className="thumbnail" src={project.thumbnailUrl} />
+                <div className="head">
+                  {project.title}&nbsp;&nbsp;
+                  {project.links && <IconButtonBar links={project.links} />}
                 </div>
-              )}
-              <div className="description">{project.description}</div>
+                <div className="affiliation">{project.affiliation}</div>
+                {project.techStack && (
+                  <div className="tech-stack">
+                    {project.techStack.map((tech, index) => (
+                      <div key={index} className="tech">
+                        {tech}
+                      </div>
+                    ))}
+                  </div>
+                )}
+                <div className="sub-title">{project.subTitle}</div>
+                <ul className="project-description">
+                  {project.descriptions.map((description, index) => {
+                    const splitIndex = description.indexOf(':');
+                    if (splitIndex !== -1) {
+                      const beforeColon = description.slice(0, splitIndex + 1);
+                      const afterColon = description.slice(splitIndex + 1);
+                      return (
+                        <li className="description" key={index}>
+                          <strong>{beforeColon}</strong>
+                          {afterColon}
+                        </li>
+                      );
+                    }
+                    return (
+                      <li className="description" key={index}>
+                        {description}
+                      </li>
+                    );
+                  })}
+                </ul>
+                <div className="description">{project.description}</div>
+              </div>
             </div>
-          </div>
-        ),
-      )}
+          ),
+        )}
+      </div>
     </div>
   );
 }
