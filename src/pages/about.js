b76bda2504fd209d95b91fb1b93ee7f05541a772
jooyoung <jooyoung.dev@gmail.com>
2026-08-02 00:06:42 +0900

    docs: About Introduce 섹션 제거 및 직함 표기 '프론트엔드 엔지니어'로 통일

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,17 +5,15 @@ import Seo from '../components/seo';
 import Bio from '../components/bio';
 import TimeStampSection from '../components/timestamp-section';
 import ProjectSection from '../components/project-section';
-import IntroduceSection from '../components/introduce-section';
 
 function AboutPage({ data }) {
   const metaData = data.site.siteMetadata;
   const { author, about, language } = metaData;
-  const { timestamps, projects, introduce } = about;
+  const { timestamps, projects } = about;
   return (
     <Layout>
       <Seo title="소개 | 조이 기술 블로그, 조이 테크" />
       <Bio author={author} language={language} />
-      <IntroduceSection title="Introduce." descriptions={introduce} />
       <TimeStampSection title="Work Experience." timestamps={timestamps} />
       <ProjectSection title="Personal Projects." projects={projects} />
     </Layout>
@@ -46,8 +44,6 @@ export const pageQuery = graphql`
         }
 
         about {
-          introduce
-
           timestamps {
             activity
             activityDescription
